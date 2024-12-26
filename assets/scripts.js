2da06c4fa130e55c685302492bb76895fbf0f432
Djibril <utilisateur@MacBook-Pro-de-Utilisateur.local>
2024-12-26 07:02:16 +0100

    modification

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    $(".gallery").mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3,
-        },
-        lightBox: true,
-        lightboxId: "myAwesomeLightbox",
-        showTags: true,
-        tagsPosition: "top",
-    });
-
-    // Optionnel : Montrez la galerie après l'initialisation
-    $(".gallery").css("display", "block");
-});
+document.addEventListener("DOMContentLoaded",function(){$(".gallery").mauGallery({columns:{xs:1,sm:2,md:3,lg:3,xl:3},lightBox:!0,lightboxId:"myAwesomeLightbox",showTags:!0,tagsPosition:"top"}),$(".gallery").css("display","block")});
